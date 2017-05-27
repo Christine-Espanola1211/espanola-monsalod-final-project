74dbaccdcb1d2b2c612b2ad8bfd9337e0f870be5
Christine-Espanola1211 <christine.espanola@gmail.com>
2017-05-27 13:52:18 +0800

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,4 @@
+document.querySelector("#welcome").innerHTML = "<button class='btn btn-primary' id='showconfirm'>WELCOME MESSAGE</button>";
+$('#showconfirm').click(function(){
+alert ("Thank you for downloading! :) \n\n Welcome to DOSCST San Isidro Campus mobile brochure app. \n\n Invest your future with DOSCST-SIC, the school built for YOU!" )
+});
